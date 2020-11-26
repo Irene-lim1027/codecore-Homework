d1650b11aaf9a8f2b5f43d0bb0ba4285b011b0ca
Irene-Lim <irenelim1027@gmail.com>
2020-11-25 18:14:01 -0800

    final

---
+++ blog_on_rails/blog_on_rails/app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+    def create
+        @post = Post.find params[:post_id]
+        @comment = Comment.new params.require(:comment).permit(:body)
+        @comment.post = @post
+
+        if @comment.save
+            redirect_to post_path(@post)
+        else
+            render post_path  
+            #posts#show
+        end
+    end
+
+    def destroy  #Why???? No route matches [GET] "/posts/2/comments/1"
+        @comment = Comment.find params[:id]
+        @comment.destroy
+        redirect_to post_path(@comment.post)
+    end
+        #posts#show go back to comment/post
+end
